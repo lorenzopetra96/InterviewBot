d8467d312e8b378b15b400e8c6fb6c0c0c4e3281
lorenzopetra96 <lorenzopetra@gmail.com>
2021-02-24 21:43:53 +0100

    config db aggiornato

--- interview-hr-bot-src/connectionpool.js
+++ interview-hr-bot-src/connectionpool.js
@@ -1,15 +1,5 @@
 const sql = require('mssql');
-
-const config = {
-    user: 'lorenzopetra96',
-    password: 'Napoletano96!',
-    server: 'hrdb01.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
-    database: 'hrdb01',
-    "options": {
-        "encrypt": true,
-        "enableArithAbort": true
-        }
-}
+const config = require('./dbconfig');
 
 var connection = new sql.ConnectionPool(config);
 
@@ -17,7 +7,6 @@ connection.connect((err)=>{
     if(err) return console.log('Could not create DB Connection!');
     console.log('Successfully Connected to Database!');  
 });
-
 
 
 module.exports = connection;
